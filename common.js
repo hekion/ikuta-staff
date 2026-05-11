7818b4af73a6be7391436a7ebbd496a10bf2ba95
hekion <yuta9925@icloud.com>
2026-05-11 22:24:34 +0900

    Refactor footer section and remove comments

--- common.js
+++ common.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Font Awesomeの読み込み（最新版）
     const fontAwesome = document.createElement('link');
     fontAwesome.rel = 'stylesheet';
     fontAwesome.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css';
@@ -45,42 +44,37 @@ document.addEventListener("DOMContentLoaded", function() {
                 <div class="footer-section">
                     <h3>KAWASAKI FRONTALE</h3>
                     <div class="sns-icons">
-                        <a href="https://www.frontale.co.jp/" target="_blank" title="公式サイト"><i class="fas fa-globe"></i></a>
-                        <a href="https://www.youtube.com/user/frontalechannel" target="_blank" title="YouTube"><i class="fab fa-youtube"></i></a>
-                        <a href="https://x.com/frontale_staff" target="_blank" title="X"><i class="fab fa-x-twitter"></i></a>
-                        <a href="https://www.instagram.com/kawasaki_frontale/" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a>
-                        <a href="https://www.tiktok.com/@frontale.official" target="_blank" title="TikTok"><i class="fab fa-tiktok"></i></a>
-                        <a href="https://page.line.me/frontale" target="_blank" title="LINE"><i class="fab fa-line"></i></a>
-                        <a href="https://www.facebook.com/kawasakifrontaleofficial" target="_blank" title="Facebook"><i class="fab fa-facebook"></i></a>
+                        <a href="https://www.frontale.co.jp/" target="_blank"><i class="fas fa-globe"></i></a>
+                        <a href="https://www.youtube.com/user/frontalechannel" target="_blank"><i class="fab fa-youtube"></i></a>
+                        <a href="https://x.com/frontale_staff" target="_blank"><i class="fab fa-x-twitter"></i></a>
+                        <a href="https://www.instagram.com/kawasaki_frontale/" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.tiktok.com/@frontale.official" target="_blank"><i class="fab fa-tiktok"></i></a>
+                        <a href="https://page.line.me/frontale" target="_blank"><i class="fab fa-line"></i></a>
+                        <a href="https://www.facebook.com/kawasakifrontaleofficial" target="_blank"><i class="fab fa-facebook"></i></a>
                     </div>
                 </div>
-
                 <div class="footer-section">
                     <h3>OFFICIAL FACILITIES</h3>
                     <ul class="facility-list">
-                        <li>
-                            <span class="fac-name">フロンタウン生田</span>
+                        <li><span class="fac-name">フロンタウン生田</span>
                             <div class="fac-links">
                                 <a href="https://ikuta.frontown.com/" target="_blank"><i class="fas fa-globe"></i></a>
                                 <a href="https://x.com/frontown_ikuta" target="_blank"><i class="fab fa-x-twitter"></i></a>
                             </div>
                         </li>
-                        <li>
-                            <span class="fac-name">フロンタウンさぎぬま</span>
+                        <li><span class="fac-name">フロンタウンさぎぬま</span>
                             <div class="fac-links">
                                 <a href="https://saginuma.frontown.com" target="_blank"><i class="fas fa-globe"></i></a>
                                 <a href="https://x.com/frontale_futsal" target="_blank"><i class="fab fa-x-twitter"></i></a>
                             </div>
                         </li>
-                        <li>
-                            <span class="fac-name">富士通スタジアム川崎</span>
+                        <li><span class="fac-name">富士通スタジアム川崎</span>
                             <div class="fac-links">
                                 <a href="https://kawasaki-fujimi.com/" target="_blank"><i class="fas fa-globe"></i></a>
                                 <a href="https://x.com/kawasaki_fujimi" target="_blank"><i class="fab fa-x-twitter"></i></a>
                             </div>
                         </li>
-                        <li>
-                            <span class="fac-name">富士見公園</span>
+                        <li><span class="fac-name">富士見公園</span>
                             <div class="fac-links">
                                 <a href="https://fujimi-park.com" target="_blank"><i class="fas fa-globe"></i></a>
                                 <a href="https://x.com/fujimi_X" target="_blank"><i class="fab fa-x-twitter"></i></a>
@@ -89,12 +83,9 @@ document.addEventListener("DOMContentLoaded", function() {
                     </ul>
                 </div>
             </div>
-            <div class="footer-bottom">
-                <p class="copyright">&copy; 2026 FRONTOWN IKUTA</p>
-            </div>
+            <div class="footer-bottom"><p class="copyright">&copy; 2026 FRONTOWN IKUTA</p></div>
         </div>
-    </footer>
-    `;
+    </footer>`;
 
     document.body.insertAdjacentHTML('afterbegin', headerHtml);
     document.body.insertAdjacentHTML('beforeend', footerHtml);
